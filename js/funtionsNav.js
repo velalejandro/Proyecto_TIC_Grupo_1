71dd7b62afde3b590b4929cdd1422d466af4b6ae
Natalia <natalialuqu@gmail.com>
2021-10-07 15:13:14 -0500

    Add barra y cambios responsive tabla

--- js/funtionsNav.js
+++ js/funtionsNav.js
@@ -1,3 +1,4 @@
+//Boton de menu
 (function () {
 	$('.menu-wrapper').on('click', function() {
 		$('.hamburger-menu').toggleClass('animate');
@@ -8,3 +9,10 @@ const btnToggle = document.querySelector('.menu-wrapper');
 btnToggle.addEventListener('click', function (){
     document.getElementById('herramientas').classList.toggle('active');
 });
+
+//Fecha Actual
+var fecha = new Date();
+var options = { year: 'numeric', month: 'long', day: 'numeric' };
+document.getElementById("fecha").innerHTML = fecha.toLocaleDateString("es-ES", options);
+	
+
